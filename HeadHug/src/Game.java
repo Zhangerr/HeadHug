119f2771289848e861a3adb6556e82ace9dbaf42
Alex Zhang <alex.zhang@berkeley.edu>
2015-01-29 22:46:58 -0800

    initial commit

---
+++ HeadHug/src/Game.java
@@ -0,0 +1,9 @@
+/**
+ * Created by azhang on 1/29/15.
+ */
+public class Game {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("what");
+    }
+}
